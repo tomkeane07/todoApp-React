589fbf33f662a4233f1e069a34edbb20ac2501e3
tom keane <tomkeane07@gmail.com>
2019-09-12 01:21:05 +0100

    simple single page REACT app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Todos from './todos';
+import Addtodo from './Addtodo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//state stores todo's
+class App extends Component {
+
+
+  state = {
+    todos: [
+      {id: 1, content: 'get up' },
+      {id: 2, content:'brush teeth' }
+    ]
+  }
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo =>{
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+      //equivalent to todos: todos when they share
+    })
+  }
+
+
+addTodo = (todo) =>{
+  todo.key = todo.id = this.state.todos.length+1;
+  let todos = [...this.state.todos, todo];
+  this.setState({
+     todos
+  })
+
+  console.log(todo);
+}
+
+
+
+  render() {
+    return (
+      <div className="App container">
+        <h1 className="center blue-text">todoApp</h1>
+        <p>click to delete</p>
+        <Todos todos={ this.state.todos} deleteTodo={this.deleteTodo}  />
+        <Addtodo addTodo={this.addTodo} />
+
+      </div>
+    );
+  }
 }
 
 export default App;
